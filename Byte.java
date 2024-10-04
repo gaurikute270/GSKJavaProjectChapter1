0a8b8ceebe8e1e955ae60745afdc74d0982f154b
Gauri kute <144440470+gaurikute270@users.noreply.github.com>
2024-10-04 00:41:04 -0700

    Add files via upload

---
+++ Byte.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class Byte {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 20;
+		System.out.println("value of byte is "+b);
+
+	}
+
+}
