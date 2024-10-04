2c2f97cea97e06e9b569a06a329c05b755601bac
Gauri kute <144440470+gaurikute270@users.noreply.github.com>
2024-10-04 00:42:17 -0700

    Add files via upload

---
+++ vari3.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class vari3 {
+     int x =20;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		vari3 t = new vari3();
+		vari3 s = new vari3();
+		vari3 y = new vari3();
+		x = x + 5;
+		t.x = s.x + 5;
+		System.out.println(t.x);
+	}
+
+}
