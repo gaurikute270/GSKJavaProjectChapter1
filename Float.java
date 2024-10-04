49eebe531c21d9d3cd5567bb32ffa40b583d932a
Gauri kute <144440470+gaurikute270@users.noreply.github.com>
2024-10-04 00:37:08 -0700

    Add files via upload

---
+++ Float.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class Float {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float x = 3.14;
+		System.out.println("value of float is "+x);
+
+	}
+
+}
